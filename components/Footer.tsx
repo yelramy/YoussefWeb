d8a3b5aa86cf28b1908cae80b1168e87afeaba57
bhannik <98061336+yelramy@users.noreply.github.com>
2025-07-23 16:47:06 -0400

    Improve design and add dynamic year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="py-6 text-center text-gray-500 text-sm border-t border-gray-800">
-      © 2025 Youssef El Ramy — All rights reserved.
+      © {year} Youssef El Ramy — All rights reserved.
     </footer>
   );
 }
